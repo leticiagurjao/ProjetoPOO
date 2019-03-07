ddc3a8abe18cc915324712e181d052c1614e6d5d
04030234 <leticia.gurjao16@hotmail.com>
2019-03-07 20:33:47 -0300

    Initial commit

---
+++ src/Crianca.java
@@ -0,0 +1,15 @@
+class Crianca {
+    
+    String corCabelo;
+    String corPele;
+    double altura;
+    int idade;
+
+    void brincar(){
+        System.out.println("Brincando...");
+    }
+    void correr(double metros){
+        System.out.println("Correndo "+ metros + " metros");
+    }
+
+}
